64e5270eade624dbc90196bd971dd38b55bceff9
IhtishamUl-haq <65214059+IhtishamUl-haq@users.noreply.github.com>
2025-11-10 22:33:46 +0500

    added pages

---
+++ CareSync/Pages/Patient/Profile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CareSync.Pages.Patient
+{
+    public class ProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
