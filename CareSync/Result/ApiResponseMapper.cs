99549da45278d5c1fe46c1df1148bf0130b28b8d
SajidKhanMarwat <msajidmarwat@gmail.com>
2025-11-11 22:01:49 +0500

    Added api response parser from json to meaningful object

---
+++ CareSync/Result/ApiResponseMapper.cs
@@ -0,0 +1,18 @@
+﻿using CareSync.Models;
+using System.Text.Json;
+
+namespace CareSync.Result;
+
+internal static class ApiResponseMapper
+{
+    public static ApiResponse<LoginResponse> MapLoginResponse(string json)
+    {
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        return JsonSerializer.Deserialize<ApiResponse<LoginResponse>>(json, options)!;
+    }
+
+}
