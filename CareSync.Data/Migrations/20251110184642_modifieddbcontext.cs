4c462d1159f6f6e2fdffc424649cd20f9138200b
SajidKhanMarwat <msajidmarwat@gmail.com>
2025-11-17 22:49:56 +0500

    gitignore

---
+++ CareSync.Data/Migrations/20251110184642_modifieddbcontext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CareSync.DataLayer.Migrations;
+
+/// <inheritdoc />
+public partial class modifieddbcontext : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+
+    }
+}
